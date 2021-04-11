5b09fbae5ef5939e1c950bd8aeb3b8ec5fbc479f
Jan Mojžíš <jan.mojzis@gmail.com>
2021-04-11 08:25:58 +0200

    filetype.c add tgz, mov

--- filetype.c
+++ filetype.c
@@ -36,6 +36,7 @@ int filetype(const char *fn,stralloc *contenttype)
     else if (case_equals(x,".bz2")) result = "application/x-bzip2";
     else if (case_equals(x,".7z"))  result = "application/x-7z-compressed";
     else if (case_equals(x,".xz"))  result = "application/x-xz";
+    else if (case_equals(x,".tgz"))  result = "application/x-gtar-compressed";
     /* documents */
     else if (case_equals(x,".dvi")) result = "application/x-dvi";
     else if (case_equals(x,".ps"))  result = "application/postscript";
@@ -53,6 +54,7 @@ int filetype(const char *fn,stralloc *contenttype)
     else if (case_equals(x,".avi")) result = "video/x-msvideo";
     else if (case_equals(x,".mp4")) result = "video/mp4";
     else if (case_equals(x,".ogg")) result = "video/ogg";
+    else if (case_equals(x,".mov")) result = "video/quicktime";
     else if (case_equals(x,".webm")) result = "video/webm";
 
     if (!stralloc_cats(contenttype, result)) return 0;
