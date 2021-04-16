02ad7e620369f9b7e03839062583c65f43f3b2c2
Jan Mojžíš <jan.mojzis@gmail.com>
2021-04-16 17:21:52 +0200

    makefilegen.sh fix bashism

--- makefilegen.sh
+++ makefilegen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 (
   (
@@ -24,8 +24,8 @@
 
     for file in `ls *.c`; do
       (
-        gcc -I../BearSSL/inc -MM "${file}"
-        echo -e "\t\$(CC) \$(CFLAGS) \$(CPPFLAGS) -c ${file}"
+        gcc -MM "${file}"
+        echo "	\$(CC) \$(CFLAGS) \$(CPPFLAGS) -c ${file}"
         echo 
       )
     done
@@ -34,21 +34,21 @@
       if grep '^int main(' "${file}" >/dev/null; then
         x=`echo "${file}" | sed 's/\.c$//'`
         echo "${x}: ${x}.o ${objects}"
-        echo -e "\t\$(CC) \$(CFLAGS) \$(CPPFLAGS) -o ${x} ${x}.o ${objects} \$(LDFLAGS)"
+        echo "	\$(CC) \$(CFLAGS) \$(CPPFLAGS) -o ${x} ${x}.o ${objects} \$(LDFLAGS)"
         echo 
       fi
     done
 
     echo "rts.out: ${binaries} rts.tests"
-    echo -e "\tsh rts.tests > rts.out"
+    echo "	sh rts.tests > rts.out"
     echo
 
     echo "rts: rts.exp rts.out"
-    echo -e "\tdiff rts.exp rts.out"
+    echo "	diff rts.exp rts.out"
     echo
 
     echo "clean:"
-    echo -e "\trm -f *.o ${binaries} rts.out"
+    echo "	rm -f *.o ${binaries} rts.out"
     echo 
 
   ) > Makefile
